94b171e8482c2a98215b986cc6b97ec2ac2f9ca6
Ketan Bhailikar <81752891+KetanBhailikar@users.noreply.github.com>
2023-04-30 14:34:57 +0530

    Completed the Visualization Module

--- visualizer.py
+++ visualizer.py
@@ -10,13 +10,27 @@ class Visualizer:
         width = 400 - margin*2
         height = 400 - margin * 2
         
-
-
-        Visualizer.drawLevel(screen, network.levels[0], left, top, width, height)
-    
-    def drawLevel(screen, level, left, top, width, height):
+        levelHeight = height/len(network.levels)
+        
         # draw the black background
         pygame.draw.rect(screen, (0,0,0), pygame.Rect(200,0,400,400))
+        
+        for i in range(len(network.levels)-1, -1, -1):
+            if len(network.levels) == 1:
+                levelTop = top + lerp(height - levelHeight, 0, 0.5)
+            else:
+                levelTop = top + lerp(height - levelHeight, 0, i/(len(network.levels)-1))
+            
+            if i == len(network.levels) - 1:
+                Visualizer.drawLevel(screen, network.levels[i], left, levelTop, width, levelHeight, ['🠉','🠈','🠊','🠋'])
+
+            Visualizer.drawLevel(screen, network.levels[i], left, levelTop, width, levelHeight, [])
+
+
+        
+    
+    def drawLevel(screen, level, left, top, width, height, outputLables):
+       
         right = left + width
         bottom = top + height
 
@@ -36,12 +50,15 @@ class Visualizer:
             x = Visualizer.getNodeX(inputs, i, left, right) + 200
             pygame.draw.circle(screen, (0,0,0), (x, bottom), nodeRadius)
             pygame.draw.circle(screen, getRGBA(inputs[i]), (x, bottom), nodeRadius * 0.6)
+            
 
         for i in range(len(outputs)):
+                # Draw the text surface onto the Pygame display surface at coordinates (100, 100)
             x = (Visualizer.getNodeX(outputs, i, left, right)) + 200
             pygame.draw.circle(screen, (0,0,0), (x, top), nodeRadius)
             pygame.draw.circle(screen, getRGBA(outputs[i]), (x, top), nodeRadius * 0.6)
             pygame.draw.circle(screen, getRGBA(biases[i]), (x, top), nodeRadius * 0.8, 2)
+        
 
     def getNodeX(nodes, index, left, right):
         return lerp(left, right, index/(len(nodes) - 1))
